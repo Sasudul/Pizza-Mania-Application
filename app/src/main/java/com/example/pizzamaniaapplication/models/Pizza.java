f79bb10e9aa698232e80bdaa9218daf0f1bf2ef6
Ishara Lakshan <isharalankshan7@gmail.com>
2025-09-16 11:44:24 +0530

    Commit Pizza details 2

---
+++ app/src/main/java/com/example/pizzamaniaapplication/models/Pizza.java
@@ -0,0 +1,24 @@
+
+package com.example.pizzamaniaapplication.models;
+
+public class Pizza {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private String imageUrl;
+
+    public Pizza(int id, String name, String description, double price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public double getPrice() { return price; }
+    public String getImageUrl() { return imageUrl; }
+}
